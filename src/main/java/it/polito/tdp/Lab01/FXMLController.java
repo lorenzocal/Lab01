9c4a3f40ea8bf6aba25e82b7884dc28a14aecdcf
loren <loren@DESKTOP-1VD94O5>
2023-03-06 21:39:35 +0100

    Risoluzione lab1, vedere ultimo punto sulla complessità computazionale mutando da ArrayList a LinkedList di parole.

--- src/main/java/it/polito/tdp/Lab01/FXMLController.java
+++ src/main/java/it/polito/tdp/Lab01/FXMLController.java
@@ -7,12 +7,13 @@ import it.polito.tdp.Lab01.model.Parole;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
 	
-	Parole elenco ;
+	Parole elenco;
 
     @FXML
     private ResourceBundle resources;
@@ -25,7 +26,13 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
-
+    
+    @FXML
+    private Button btnCancella;
+    
+    @FXML
+    private Label tmpEsecuzione;
+    
     @FXML
     private TextField txtParola;
 
@@ -34,21 +41,41 @@ public class FXMLController {
 
     @FXML
     void doInsert(ActionEvent event) {
-
+    	this.elenco.addParola(txtParola.getText());
+    	txtParola.clear();
+    	txtResult.clear();
+    	for (String s : this.elenco.getElenco()) {
+    		txtResult.appendText(s + "\n");
+    	};
+    	tmpEsecuzione.setText("  " + Long.toString(System.nanoTime()));
     }
 
     @FXML
     void doReset(ActionEvent event) {
+    	this.elenco.getElenco().clear();
+    	txtResult.clear();
+    	tmpEsecuzione.setText("  " + Long.toString(System.nanoTime()));
+    }
+    
 
+    @FXML
+    void doCancella(ActionEvent event) {
+    	this.elenco.removeParola(txtResult.getSelectedText());
+    	txtResult.clear();
+    	for (String s : this.elenco.getElenco()) {
+    		txtResult.appendText(s + "\n");
+    	};
+    	tmpEsecuzione.setText("  " + Long.toString(System.nanoTime()));
     }
 
     @FXML
     void initialize() {
-        assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
+    	assert btnCancella != null : "fx:id=\"btnCancella\" was not injected: check your FXML file 'Scene.fxml'.";
+    	assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert tmpEsecuzione != null : "fx:id=\"tmpEsecuzione\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
-        
         elenco = new Parole() ;
     }
 
--- src/main/java/it/polito/tdp/Lab01/model/Parole.java
+++ src/main/java/it/polito/tdp/Lab01/model/Parole.java
@@ -1,24 +1,40 @@
 package it.polito.tdp.Lab01.model;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Parole {
+	
+	private List<String> parole;
 		
 	public Parole() {
-		//TODO
+		this.parole = new ArrayList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		parole.add(p);
+	}
+	
+	public void removeParola(String p) {
+		parole.remove(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		Collections.sort(this.parole);
+		return this.parole;
 	}
 	
 	public void reset() {
-		// TODO
+		this.parole.clear();
+	}
+	
+	public String toString() {
+		String s1 = "";
+		for (String s : this.parole) {
+			s1 = s1 + s + "\n";
+		}
+		return s1;
 	}
 
 }
